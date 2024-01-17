99407f9b6c531ee5552fbc3d9187941a69abab8e
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-17 03:36:27 +0100

    Add expense model

---
+++ app/models/expense.rb
@@ -0,0 +1,12 @@
+class Expense < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :category
+
+  
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :category_id, presence: true
+
+  def most_recent
+  end
+end
