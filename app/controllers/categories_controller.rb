89fa1a80c0e3143a8a71b872fee89807b3ccd172
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-18 02:25:38 +0100

    Update index & new actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,12 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @categories = current_user.categories
   end
 
   def new
+    @category = Category.new
   end
 
   def create
