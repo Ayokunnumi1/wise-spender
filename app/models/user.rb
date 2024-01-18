3c625ab53828cb20e5349f54ae4a55314fc995cb
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-18 02:26:10 +0100

    Include relationships with category & expenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    has_many :categories 
+    has_many :expenses, through: :categories    
+    
     validates :name, presence: true
 end
