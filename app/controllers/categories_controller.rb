71a50467c1f1bd437bf1857dc73fa477d24679f5
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-18 15:08:12 +0100

    Include edit, update & destroy actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,5 +10,37 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      flash[:success] = 'Category Saved Successfully'
+      redirect_to categories_path
+    else
+      flash[:error] = 'Category Not Saved, try later'
+      render :new
+    end
   end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+  @category = Category.find(params[:id])
+  if @category.update(category_params)
+    redirect_to categories_path, notice: 'Category was successfully updated.'
+  else
+    render :edit
+  end
+ end
+
+ def destroy
+  @category = Category.find(params[:id])
+  @category.destroy
+  redirect_to categories_path, notice: 'Category was successfully deleted.'
+ end
+  
 end
