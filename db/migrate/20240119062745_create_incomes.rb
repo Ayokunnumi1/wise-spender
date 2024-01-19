6ab7f51f26df10579c50268bfd41fa61258de79d
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-19 12:57:48 +0100

    Update income migration file

--- db/migrate/20240119062745_create_incomes.rb
+++ db/migrate/20240119062745_create_incomes.rb
@@ -1,6 +1,7 @@
 class CreateIncomes < ActiveRecord::Migration[7.1]
   def change
     create_table :incomes do |t|
+      t.decimal "amount", precision: 10, scale: 2
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
