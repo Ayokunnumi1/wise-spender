8cf5db76bab72415098a61e79c2a10338ee3ef5e
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-19 12:52:21 +0100

    Update income validations in income model

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,5 +1,5 @@
 class Income < ApplicationRecord
   belongs_to :user
 
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
