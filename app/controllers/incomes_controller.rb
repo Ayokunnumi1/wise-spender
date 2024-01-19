7601175c05f6b27151bbf0abda7a66071d481a73
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-19 12:51:02 +0100

    Update new & create actions in income controller

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,7 +1,28 @@
 class IncomesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @income = current_user.incomes.build
   end
 
   def create
+    @income = current_user.incomes.build(income_params)
+
+    if @income.save
+      redirect_to categories_path, notice: 'Income Added Successfully.'
+    else
+      render :new
+    end
+  end
+
+  def clear_all
+    current_user.incomes.destroy_all
+    redirect_to categories_path, notice: 'All incomes cleared successfully.'
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:amount)
   end
 end
