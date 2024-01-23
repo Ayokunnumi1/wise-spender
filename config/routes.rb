8facaa73578202d3ffcac07c74a1b7db151ab6d6
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-23 21:19:00 +0100

    Fix css precompile error & make flashes index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "categories#index"
+  root "flashes#index"
   resources :users, only: [:index]
   resources :expenses, only: [:new, :create, :edit, :update, :destroy, :show]
   resources :categories, only: [:index, :new, :create, :edit, :update, :destroy, :show] 
@@ -19,4 +19,12 @@ Rails.application.routes.draw do
       post 'clear_all'
     end
   end 
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+  
+  unauthenticated do
+    root 'flashes#index', as: :unauthenticated_root
+  end
 end
