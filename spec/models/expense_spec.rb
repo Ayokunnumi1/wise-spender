0fc1594325446d23923f7994a114684e6619b2db
Ayokunnumi1 <omololuayk@gmail.com>
2024-01-27 15:13:26 +0100

    Add test files & codes for all models

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,49 @@
+# spec/models/expense_spec.rb
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do 
+    user = User.new(name: 'Test User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+    user.skip_confirmation_notification!
+    user.save
+    user
+  end
+  let(:category) { Category.create(name: 'Test Category', icon: 'test_icon', user: user) }
+
+  context 'validations' do
+    it 'validates presence and numericality of amount' do
+      expense = Expense.new(name: 'Test Expense', amount: nil, author_id: user.id, category_id: category.id)
+      expect(expense.valid?).to be_falsey
+      expect(expense.errors[:amount]).to include("can't be blank", "is not a number")
+
+      expense.amount = -1
+      expect(expense.valid?).to be_falsey
+      expect(expense.errors[:amount]).to include("must be greater than or equal to 0")
+
+      expense.amount = 1.5
+      expect(expense.valid?).to be_falsey
+      expect(expense.errors[:amount]).to include("must be an integer")
+
+      expense.amount = 1000
+      expect(expense.valid?).to be_truthy
+    end
+
+    it 'validates presence of name' do
+      expense = Expense.new(name: nil, amount: 1000, author_id: user.id, category_id: category.id)
+      expect(expense.valid?).to be_falsey
+      expect(expense.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  context 'associations' do
+    let(:expense) { Expense.create(name: 'Test Expense', amount: 1000, author_id: user.id, category_id: category.id) }
+
+    it 'belongs to author' do
+      expect(expense.author).to eq(user)
+    end
+
+    it 'belongs to category' do
+      expect(expense.category).to eq(category)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
+# spec/models/user_spec.rb
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'validates presence of name' do
+      user = User.new(name: nil)
+      expect(user.valid?).to be_falsey
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  context 'associations' do
+    let(:user) { User.new(name: 'Test User', email: 'test@example.com', password: 'password', password_confirmation: 'password') }
+    let(:category) { Category.create(name: 'Test Category', user: user) }
+    let(:expense) { Expense.create(name: 'Test Expense', category: category) }
+    let(:income) { Income.create(amount: 1000, user: user) } # Changed 'name' to 'amount'
+
+    before do
+      # Disable sending confirmation email
+      user.skip_confirmation_notification!
+      user.save
+    end
+
+    it 'has many categories' do
+      user.categories << category
+      expect(user.categories).to include(category)
+    end
+
+    it 'has many expenses through categories' do
+      user.categories << category
+      category.expenses << expense
+      expect(user.expenses).to include(expense)
+    end
+
+    it 'has many incomes' do
+      user.incomes << income
+      expect(user.incomes).to include(income)
+    end
+  end
 end
